fc8a13c98d9d368c88e454b625437f7a90ca0639
Hai Nguyen <haind.vng@gmail.com>
2020-03-20 21:21:45 +0700

    update config

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -1,8 +1,16 @@
 package com.example.consumer;
 
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
+
+@Log4j2
+@EnableBinding(Sink.class)
 @SpringBootApplication
 public class ConsumerApplication {
 
@@ -10,4 +18,13 @@ public class ConsumerApplication {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
+    @StreamListener(Sink.INPUT)
+    public void onSubscribe(PersonEvent personEvent) {
+        log.info("Receive: {}", personEvent);
+    }
+}
+
+@Data
+class PersonEvent {
+    private String name;
 }
--- producer/src/main/java/com/example/producer/ProducerApplication.java
+++ producer/src/main/java/com/example/producer/ProducerApplication.java
@@ -1,8 +1,19 @@
 package com.example.producer;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 
+@Log4j2
+@EnableBinding(Source.class)
 @SpringBootApplication
 public class ProducerApplication {
 
@@ -10,4 +21,18 @@ public class ProducerApplication {
         SpringApplication.run(ProducerApplication.class, args);
     }
 
+
+    @Bean
+    public ApplicationRunner onPublish(Source source) {
+        return args -> {
+            source.output().send(MessageBuilder.withPayload(new PersonEvent("Arya")).build());
+            log.info("send event completed!");
+        };
+    }
+}
+
+@Data
+@AllArgsConstructor
+class PersonEvent {
+    private String name;
 }

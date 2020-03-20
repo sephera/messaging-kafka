fbca0bf1e354068d7e8da5446c2648e8cec482b5
Hai Nguyen <haind.vng@gmail.com>
2020-03-20 13:53:26 +0700

    add consumer producer

---
+++ stream-consumer/src/main/java/com/example/streamconsumer/StreamConsumerApplication.java
@@ -0,0 +1,33 @@
+package com.example.streamconsumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+
+@EnableBinding(Sink.class)
+@Log4j2
+@SpringBootApplication
+public class StreamConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumerApplication.class, args);
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void consumeEmployeeDetails(Person person) {
+        log.info("Let's process employee details: {}", person);
+    }
+}
+
+
+@Data
+@AllArgsConstructor
+class Person {
+    private String name;
+}
